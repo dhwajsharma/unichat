d13f1c29efaf0e52b462a9d6a0483239bc002ada
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-01-03 19:10:43 +0530

    working on login component

---
+++ src/components/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+
+const Login = () => {
+  return <div className="">login</div>;
+};
+
+export default Login;
